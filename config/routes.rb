1055a5121858cf3301b19102f451a3afb4def20f
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-10-17 11:42:19 +0000

    korekara user ichiran

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  get "homes/about" => "homes#about", as: "about"
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
+  resources :users, only: [:show, :edit, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
